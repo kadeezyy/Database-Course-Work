dc2c7b1aa5a7f457869fb14af394f4fc9ec72d10
kanat <kanttoroev@gmail.com>
2023-11-15 23:41:14 +0300

    Initiated directory for database queries

--- Music-platform/src/main/resources/database/create.sql
+++ Music-platform/src/main/resources/database/create.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS role (
     description TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS custom_user (
     id UUID PRIMARY KEY,
     role_id UUID NOT NULL,
     username VARCHAR(128) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS user (
         FOREIGN KEY (role_id)
             REFERENCES role (id)
 );
-CREATE INDEX ON user (username);
+CREATE INDEX ON custom_user (username);
 
 CREATE TABLE IF NOT EXISTS genre (
      id UUID PRIMARY KEY,
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS artist (
     nickname VARCHAR(128) UNIQUE NOT NULL,
     CONSTRAINT user_id_fk
         FOREIGN KEY (user_id)
-            REFERENCES user (id)
+            REFERENCES custom_user (id)
 );
 CREATE INDEX ON artist (nickname);
 
@@ -54,10 +54,10 @@ CREATE TABLE IF NOT EXISTS album (
     creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     songs_count INT NOT NULL,
     CONSTRAINT artist_id_fk
-        FOREIGN KEY artist_id
+        FOREIGN KEY (artist_id)
             REFERENCES artist (id),
     CONSTRAINT genre_id_fk
-        FOREIGN KEY genre_id
+        FOREIGN KEY (genre_id)
             REFERENCES genre (id)
 );
 CREATE INDEX ON album (title);
@@ -69,8 +69,8 @@ CREATE TABLE IF NOT EXISTS playlist (
     creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     songs_count INT NOT NULL,
     CONSTRAINT user_creator_id_fk
-        FOREIGN KEY user_creator_id
-            REFERENCES user (id)
+        FOREIGN KEY (user_creator_id)
+            REFERENCES custom_user (id)
 );
 CREATE INDEX ON playlist (title);
 
@@ -78,10 +78,10 @@ CREATE TABLE IF NOT EXISTS playlist_songs (
     playlist_id UUID NOT NULL,
     song_id UUID NOT NULL,
     CONSTRAINT playlist_id_fk
-        FOREIGN KEY playlist_id
+        FOREIGN KEY (playlist_id)
             REFERENCES playlist (id),
     CONSTRAINT song_id_fk
-        FOREIGN KEY song_id
+        FOREIGN KEY (song_id)
             REFERENCES song (id),
     PRIMARY KEY (playlist_id, song_id)
 );
@@ -90,11 +90,11 @@ CREATE TABLE IF NOT EXISTS user_favourite_artists (
     user_id UUID NOT NULL,
     artist_id UUID NOT NULL,
     CONSTRAINT artist_id_fk
-        FOREIGN KEY artist_id
+        FOREIGN KEY (artist_id)
             REFERENCES artist (id),
     CONSTRAINT user_id_fk
-        FOREIGN KEY user_id
-            REFERENCES user (id),
+        FOREIGN KEY (user_id)
+            REFERENCES custom_user (id),
     PRIMARY KEY (user_id, artist_id)
 );
 
@@ -102,10 +102,10 @@ CREATE TABLE IF NOT EXISTS user_liked_songs (
     user_id UUID NOT NULL,
     song_id UUID NOT NULL,
     CONSTRAINT user_id_fk
-        FOREIGN KEY user_id
-            REFERENCES user (id),
+        FOREIGN KEY (user_id)
+            REFERENCES custom_user (id),
     CONSTRAINT song_id_fk
-        FOREIGN KEY song_id
+        FOREIGN KEY (song_id)
             REFERENCES song (id),
     PRIMARY KEY (user_id, song_id)
 );
@@ -116,10 +116,10 @@ CREATE TABLE IF NOT EXISTS user_listening_history (
     date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT user_id_fk
-        FOREIGN KEY user_id
-            REFERENCES user (id),
+        FOREIGN KEY (user_id)
+            REFERENCES custom_user (id),
     CONSTRAINT song_id_fk
-        FOREIGN KEY song_id
+        FOREIGN KEY (song_id)
             REFERENCES song (id),
     PRIMARY KEY (user_id, song_id)
 );
@@ -128,10 +128,10 @@ CREATE TABLE IF NOT EXISTS user_favourite_playlists (
     user_id UUID NOT NULL,
     playlist_id UUID NOT NULL,
     CONSTRAINT user_id_fk
-        FOREIGN KEY user_id
-            REFERENCES user (id),
+        FOREIGN KEY (user_id)
+            REFERENCES custom_user (id),
     CONSTRAINT playlist_id_fk
-        FOREIGN KEY playlist_id
+        FOREIGN KEY (playlist_id)
             REFERENCES playlist (id),
     PRIMARY KEY (user_id, playlist_id)
 );
@@ -141,10 +141,10 @@ CREATE TABLE IF NOT EXISTS album_songs (
     album_id UUID NOT NULL,
     song_id UUID NOT NULL,
     CONSTRAINT album_id_fk
-        FOREIGN KEY album_id
+        FOREIGN KEY (album_id)
             REFERENCES album (id),
     CONSTRAINT song_id_fk
-        FOREIGN KEY song_id
+        FOREIGN KEY (song_id)
             REFERENCES song (id),
     PRIMARY KEY (album_id, song_id)
 );
@@ -153,10 +153,10 @@ CREATE TABLE IF NOT EXISTS artist_songs (
     artist_id UUID NOT NULL,
     song_id UUID NOT NULL,
     CONSTRAINT artist_id_fk
-        FOREIGN KEY artist_id
+        FOREIGN KEY (artist_id)
             REFERENCES artist (id),
     CONSTRAINT song_id_fk
-        FOREIGN KEY song_id
+        FOREIGN KEY (song_id)
             REFERENCES song (id),
     PRIMARY KEY (artist_id, song_id)
 );
