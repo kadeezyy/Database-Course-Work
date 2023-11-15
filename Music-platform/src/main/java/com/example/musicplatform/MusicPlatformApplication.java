bcb652d797616bd506f1a55def3500a100920525
drain shawty <hopegasgas@gmail.com>
2023-11-15 23:43:42 +0300

    comment in main class

--- Music-platform/src/main/java/com/example/musicplatform/MusicPlatformApplication.java
+++ Music-platform/src/main/java/com/example/musicplatform/MusicPlatformApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusicPlatformApplication {
-
+	//drain shawty
 	public static void main(String[] args) {
 		SpringApplication.run(MusicPlatformApplication.class, args);
 	}
