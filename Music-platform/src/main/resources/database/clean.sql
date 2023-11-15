f4d84f9593e09d23824730cfd4d2b531dd342547
kanat <kanttoroev@gmail.com>
2023-11-15 23:18:21 +0300

    Initiated directory for database queries

---
+++ Music-platform/src/main/resources/database/clean.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS song;
+DROP TABLE IF EXISTS artist;
+DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS playlist;
+DROP TABLE IF EXISTS playlist_songs;
+DROP TABLE IF EXISTS user_favourite_artists;
+DROP TABLE IF EXISTS user_liked_songs;
+DROP TABLE IF EXISTS user_listening_history;
+DROP TABLE IF EXISTS user_favourite_playlists;
+DROP TABLE IF EXISTS album_songs;
+DROP TABLE IF EXISTS artist_songs;
